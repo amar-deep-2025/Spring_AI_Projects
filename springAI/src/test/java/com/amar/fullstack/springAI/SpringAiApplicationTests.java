3a7aae523c013894a6be4820ecc46255cdbf3662
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-23 12:11:39 +0530

    Added first Spring Boot project

---
+++ springAI/src/test/java/com/amar/fullstack/springAI/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amar.fullstack.springAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
